19f995abc9047aac8136bc68ca193e94896b78a9
Marcus <juandbeta@gmail.com>
2024-03-06 19:59:20 +0100

    07: Chamber Discover Page

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -12,9 +12,9 @@ const menuButton = document.querySelector("#menuButton");
 const menu = document.querySelector("#menuOptions");
 
 menuButton.addEventListener("click", () => {
-	menuButton.classList.toggle('open');
+    menuButton.classList.toggle('open');
     menu.classList.toggle("open");
-    
+
 });
 
 darkMode.addEventListener("click", () => {
@@ -26,3 +26,44 @@ darkPcMode.addEventListener("click", () => {
     main.classList.toggle("dark");
     footer.classList.toggle("dark");
 });
+
+// Number of visits
+
+const message = document.querySelector("#message");
+const msToDays = 84600000;
+twoDays = msToDays * 2;
+
+let lastVisit = Number(window.localStorage.getItem("lastVisit-ls")) || 0;
+let nbrVisits = Number(window.localStorage.getItem("nbrVisits-ls")) || 0;
+
+let diff = (Date.now() - lastVisit);
+
+if (nbrVisits == 0) {
+
+    message.textContent = "Welcome! Let us know if you have any questions.";
+
+} else if (diff < msToDays) {
+
+    message.textContent = "Back so soon! Awesome!";
+
+}
+
+else {
+    daysAgo = ((Date.now() / msToDays) - lastVisit).toFixed(0);
+
+    if ((diff > msToDays) && (diff < twoDays)) {
+        diff = diff / msToDays;
+        message.textContent = `You last visited ${diff.toFixed(0)} day ago`;
+    } else {
+        diff = diff / msToDays;
+        message.textContent = `You last visited ${diff.toFixed(0)} days ago`;
+    }
+}
+
+nbrVisits++;
+
+lastVisit = Date.now();
+
+
+localStorage.setItem("nbrVisits-ls", nbrVisits);
+localStorage.setItem("lastVisit-ls", lastVisit);
