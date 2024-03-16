bc2a91080bbc11472c1b315691f519640a2442b6
Marcus <juandbeta@gmail.com>
2024-03-16 07:48:54 +0100

    09 Assignment: JSON Data

--- scripts/links.js
+++ scripts/links.js
@@ -14,20 +14,34 @@ const displayLinks = (weeks) => {
 
     ul = document.createElement("ul");
     //create elements
-    console.table(weeks);
+    //console.table(weeks);
     weeks.lessons.forEach((week) => {
         li = document.createElement("li");
 
+        li.textContent = `${week.lesson}. `;
+
         //populating elements
+        length = week.links.length;
+        lengthCounter = 0;
 
         week.links.forEach(link => {
 
             a = document.createElement("a");
             a.setAttribute("href", link.url);
 
-            console.table(link);
 
-            a.textContent = `${link.title} - `;
+
+            if (length != 1) {
+                a.textContent = `${link.title} | `;
+                
+                if (lengthCounter != 0 ){ 
+                    a.textContent = `${link.title}`;  
+                }
+            } else {
+                a.textContent = `${link.title}`;
+            }
+
+            lengthCounter += 1;
 
             card.appendChild(ul);
             ul.appendChild(li);
