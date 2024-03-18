2f67800076b2bdb1e7c3449a568776a773b8030d
Marcus <juandbeta@gmail.com>
2024-03-18 03:40:30 +0100

    Members of the commercial chamber page created

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,3 +1,29 @@
+//Darkmode Variables
+
+const darkMode = document.querySelector("#darkMode");
+const darkPcMode = document.querySelector("#darkPcMode");
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
+
+const menuButton = document.querySelector("#menuButton");
+const menu = document.querySelector("#menuOptions");
+
+menuButton.addEventListener("click", () => {
+    menuButton.classList.toggle('open');
+    menu.classList.toggle("open");
+
+});
+
+darkMode.addEventListener("click", () => {
+    main.classList.toggle("dark");
+    footer.classList.toggle("dark");
+});
+
+darkPcMode.addEventListener("click", () => {
+    main.classList.toggle("dark");
+    footer.classList.toggle("dark");
+});
+
 //Get and display information of members
 
 const membersInfo = "https://ALatinWebDeveloper.github.io/wdd230/chamber/data/members.json";
@@ -13,7 +39,55 @@ async function getMembers() {
 
 const displayMembers = (members) => {
 
-    console.table(members);
+    members.members.forEach(member => {
+
+        //Creating elements
+        section = document.createElement('section');
+        p = document.createElement('p');
+        address = document.createElement('p');
+        img = document.createElement('img');
+        phone = document.createElement('p');
+        website = document.createElement('p');
+        membership = document.createElement('p');
+
+        //Populating elements
+        p.textContent = member.name;
+        address.textContent = member.address;
+        phone.textContent = member.phone;
+        website.textContent = member.website;
+        membership.textContent = member.membership;
+
+        img.setAttribute('src', member.image);
+        img.setAttribute('alt', `Profile picture of ${member.name}`);
+
+        //Arming the card
+
+        membersDiv.appendChild(section);
+        section.appendChild(p);
+        section.appendChild(img);
+        section.appendChild(address);
+        section.appendChild(phone);
+        section.appendChild(website);
+        section.appendChild(membership);
+    });
+}
+
+//Grid and List buttons
+
+const gridBtn = document.querySelector('#btnGrid');
+const listBtn = document.querySelector('#btnList');
+
+listBtn.addEventListener('click', displayList);
+gridBtn.addEventListener('click', displayGrid);
+
+function displayList() {
+
+    membersDiv.classList.replace("gridFormat", 'listFormat');
+}
+
+function displayGrid() {
+
+    membersDiv.classList.replace("listFormat", 'gridFormat');
 }
 
 getMembers();
