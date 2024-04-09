916dd0bc9aec6cdbc869e35241bdbd684c7dec3e
Marcus <juandbeta@gmail.com>
2024-04-09 12:46:25 -0400

    json adjusted

--- project/scrypts/prices.js
+++ project/scrypts/prices.js
@@ -1,5 +1,5 @@
 const url = "https://ALatinWebDeveloper.github.io/wdd230/project/data/pricing.json";
-const table = document.querySelector("#pricetbl");
+const tbody = document.querySelector("tbody");
 
 async function getPrices() {
 
@@ -15,7 +15,38 @@ const displayPrices = (data) => {
 
     /*Creating the variables necessary*/
 
-    
+    data.forEach(rentalData => {
+        console.log(rentalData.fullDay.reservation);
+        let tr = document.createElement("tr");
+        let rentalType = document.createElement("td");
+        let capacity = document.createElement("td");
+
+        let reservHalfDay = document.createElement("td");
+        let reservFullDay = document.createElement("td");
+
+        let WalkInHalfDay = document.createElement("td");
+        let WalkInFullDay = document.createElement("td");
+
+        /*Populating elements*/
+
+        rentalType.textContent = rentalData.vehicle;
+        capacity.textContent = rentalData.capacity;
+        reservHalfDay.textContent = rentalData.fullDay.reservation;
+        reservFullDay.textContent = rentalData.fullDay.reservation;
+        WalkInHalfDay.textContent = "❌";
+        WalkInFullDay.textContent = "❌";
+
+        /*Appending elements*/
+
+        tbody.appendChild(tr);
+        tbody.appendChild(rentalType);
+        tbody.appendChild(capacity);
+        tbody.appendChild(reservHalfDay);
+        tbody.appendChild(reservFullDay);
+        tbody.appendChild(WalkInHalfDay);
+        tbody.appendChild(WalkInFullDay);
+    });
+
 }
 
 getPrices();
